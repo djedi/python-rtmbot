1cace0436d59ffd54cf98a4f8cd38916ebdcc7b0
Dustin Davis <dustin.davis@edgecast.com>
2022-10-07 11:52:07 -0600

    edit setup file

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='rtmbot',
-    version='0.4.1',
+    version='0.4.2',
     description='A Slack bot written in python that connects via the RTM API.',
     author='Ryan Huber and Jeff Ammons',
     author_email='developers@slack.com',
-    url='https://github.com/slackhq/python-rtmbot',
+    url='https://github.com/djedi/python-rtmbot',
     packages=find_packages(),
     entry_points={'console_scripts': ['rtmbot=rtmbot.bin.run_rtmbot:main']},
     install_requires=[
